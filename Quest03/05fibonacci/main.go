fc45fe1182b3dc1c962e4c1c163449dc05c27ce2
kokizen <koki.alemino@gmail.com>
2025-07-02 19:17:18 +0100

    -

---
+++ Quest03/05fibonacci/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"piscine-go-test/Quest03/05fibonacci/piscine"
+)
+
+func main() {
+	arg1 := 4
+	fmt.Println(piscine.Fibonacci(arg1))
+}
---
+++ Quest03/06sqrt/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"piscine-go-test/Quest03/06sqrt/piscine"
+)
+
+func main() {
+	fmt.Println(piscine.Sqrt(4))
+	fmt.Println(piscine.Sqrt(3))
+}
