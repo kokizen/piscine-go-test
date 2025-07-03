285c7e7f50b1b5df1ecc3693c269cb966cb3f2e1
kokizen <koki.alemino@gmail.com>
2025-07-03 12:41:23 +0100

    -

---
+++ Quest04/08isupper/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"piscine-go-test/Quest04/08isupper/piscine"
+)
+
+func main() {
+	fmt.Println(piscine.IsUpper("HELLO"))
+	fmt.Println(piscine.IsUpper("HELLO!"))
+
+}
